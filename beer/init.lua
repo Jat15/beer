693d736bbda3a06d53bd561abd0ffdbc5c78f7eb
AndrOn25022012 <vincentouchard@gmail.com>
2013-07-09 20:52:28 +0200

    mofify init.lua to add particles when player drank

--- beer/init.lua
+++ beer/init.lua
@@ -1,4 +1,4 @@
-
+drunklevel = {};
 
 minetest.register_craftitem("beer:cone", {
 	description = "cone",
@@ -32,16 +32,65 @@ minetest.register_craftitem("beer:empty_tankard", {
 
 
 })
+
+function decrDrunkLevel(name)
+	drunklevel[name] = drunklevel[name] - 1
+end
+
 minetest.register_craftitem("beer:beer_tankard", {
 	description = "Beer tankart",
 	inventory_image = "beer_beer_tankart.png",
 	stack_max = 1,
 	on_use = function(itemstack, user, pointed_thing)
-		minetest.sound_play("drink_a_beer")		
+		minetest.sound_play("drink_a_beer")
+		drunklevel[user:get_player_name()] = drunklevel[user:get_player_name()] + 1
+		name = user:get_player_name()
+		minetest.after(30, decrDrunkLevel,name)
 		return ItemStack("beer:empty_tankard ")
 		end
 
 })
+
+
+--------------------------------------------------
+
+minetest.register_globalstep(function(dtime)
+
+
+for _, p in ipairs(minetest.get_connected_players()) do
+		if drunklevel[p:get_player_name()] == nil
+			then
+				drunklevel[p:get_player_name()] = 0
+			end
+		if drunklevel[p:get_player_name()] >= 6
+		then
+
+			--local yaw = p:get_look_pitch()--*180/math.pi
+			--yaw = yaw + math.random(-10,10)/200			
+			--p:set_look_pitch(yaw*math.pi/180)
+			local dir = {x=0,y=0,z=0}
+			dir.x = p:get_look_dir().x *20
+			dir.y = p:get_look_dir().y *20
+			dir.z = p:get_look_dir().z *20
+			minetest.add_particle(
+					{x=p:getpos().x + dir.x/100 + math.random(-10,10)/50,
+				 	y=p:getpos().y + 1   + math.random(-10,10)/50,
+				 	z=p:getpos().z + dir.z/100 + math.random(-10,10)/50}, 
+					{x=dir.x/100 + math.random(-10,10)/100,
+					 y=dir.y/100 + 3,
+					 z=dir.z/100 + math.random(-10,10)/100},
+					 {x=0, y=-5, z=0},
+					 0.2,
+					 math.random(10,30)/10, false, "spirale.png")
+		end
+		
+		
+
+	end
+end)
+
+--------------------------------------------------
+
 
 minetest.register_craft({
 	type = "shapeless",	
