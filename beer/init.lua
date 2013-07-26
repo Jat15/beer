de5df6dcca8ecc0cb98c78bcbe7adb5a537d8b91
Jat <jinan15@hotmail.com>
2013-07-26 16:11:17 +0200

    Fix Syntax and sound

--- beer/init.lua
+++ beer/init.lua
@@ -1,20 +1,5 @@
 drunklevel = {};
 
-minetest.register_craftitem("beer:cone", {
-	description = "cone",
-	inventory_image = "beer_cone.png",
-})
-
-
-minetest.register_craftitem("beer:crude_beer_mix", {
-	description = "Crude beer mix",
-	inventory_image = "beer_crude_mix.png",
-})
-
-minetest.register_craftitem("beer:fine_beer_mix", {
-	description = "Fine beer mix",
-	inventory_image = "beer_fine_mix.png",
-})
 minetest.register_craftitem("beer:empty_tankard", {
 	description = "Empty tankart",
 	inventory_image = "beer_empty_tankart.png",
@@ -80,33 +65,12 @@ end)
 
 --------------------------------------------------
 
-
-minetest.register_craft({
-	type = "shapeless",	
-	output = "beer:crude_beer_mix",
-	recipe = {"beer:cone","bucket:bucket_water"},
-	replacements = {{"bucket:bucket_water", "bucket:bucket_empty"}}
-})
-
-minetest.register_craft({
-	type = "cooking",	
-	output = "beer:fine_beer_mix",
-	recipe = "beer:crude_beer_mix",
-	cooktime = 10,
-})
-
 minetest.register_craft({
 	output = "beer:empty_tankard 3",
 	recipe = {
 		{"default:glass",""		,"default:glass"},
 		{"default:glass","default:glass",""		},
 		{""		,""		,""		}},
-})
-
-minetest.register_craft({
-	type = "fuel",
-	recipe = "beer:cone",
-	burntime = 1
 })
 
 dofile(minetest.get_modpath("beer").."/barrel.lua")
